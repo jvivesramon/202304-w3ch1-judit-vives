0df4b8120877d798e4c1093c96ee63d603719cf3
Judit Vives <jvivesramon@gmail.com>
2023-04-17 19:25:46 +0200

    Install Eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "error",
+  },
+};
