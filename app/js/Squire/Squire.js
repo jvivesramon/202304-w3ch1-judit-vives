88a9ae3cca3d3cff8aa678f9cbb18e66f93e3ead
Judit Vives <jvivesramon@gmail.com>
2023-04-17 20:34:11 +0200

    Add Squire class at js file

---
+++ app/js/Squire/Squire.js
@@ -0,0 +1,34 @@
+import Character from "../Character/Character.js";
+import Fighter from "../Fighter/Fighter.js";
+
+class Squire extends Character {
+  kissAssLeve;
+  serves;
+
+  constructor(characterData, kissAssLevel, serves) {
+    super(characterData);
+    this.kissAssLevel = this.kissAssLevelFilter(kissAssLevel);
+
+    if (serves instanceof Fighter) {
+      this.serves = serves;
+    }
+  }
+
+  communicate() {
+    return `${super.communicate()}I'm a loser`;
+  }
+
+  #kissAssLevelFilter(kissAssLevel) {
+    if (kissAssLevel < 0) {
+      return 0;
+    }
+
+    if (kissAssLevel > 10) {
+      return 10;
+    }
+
+    return kissAssLevel;
+  }
+}
+
+export default Squire;
